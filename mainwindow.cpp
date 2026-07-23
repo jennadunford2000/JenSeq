153316c425d779793a27092a9ecd1e3f1b04f095
Jenna Dunford <jenna.dunford@bbd.co.za>
2026-07-23 17:54:05 +0200

    Rudimentary analysis added
    Can count the numbers of bases, give the total count of bases, and provide percentages of bases. Huge, I know

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,13 +4,20 @@
 #include <QFileDialog>
 #include <cstdlib>
 
+
+QList<QString> outputList;
+int GValue = 0;
+int TValue = 0;
+int CValue = 0;
+int AValue = 0;
+int total;
+int notBaseValue = 0;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QString env;
-    env = getenv("NCBI_API_KEY");
 }
 
 MainWindow::~MainWindow()
@@ -28,6 +35,7 @@ void MainWindow::on_pushButton_2_clicked()
 {
     QStringList fileNames;
     QString strng;
+    ui->Output->clear();
 
     QFileDialog dialog(this);
     dialog.setNameFilter(tr("Text files (*.txt)"));
@@ -41,13 +49,84 @@ void MainWindow::on_pushButton_2_clicked()
         inputFile.open(QIODevice::ReadOnly);
         if (!inputFile.isOpen())
             return;
-
+        int i = 0;
         QTextStream stream(&inputFile);
         for (QString line = stream.readLine();
              !line.isNull();
              line = stream.readLine()) {
-            ui->Output->insertPlainText(line + "\n");
+            if(i>0)
+            {
+                ui->Output->insertPlainText(line + "\n");
+                outputList.append(line);
+            }
+            i++;
         };
 
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    if(outputList.empty())
+    {
+        QMessageBox::about(this,"No items to analyse", "Your data is empty. Please load a file");
+    }
+    else
+    {
+        AValue = 0;
+        GValue = 0;
+        CValue = 0;
+        TValue = 0;
+        total = 0;
+        notBaseValue = 0;
+        for(int a = 0; a < outputList.length(); a++)
+        {
+            QString test = outputList.value(a);
+            for(int b = 0; b< test.length(); b++)
+            {
+                if (test.at(b) != " ")
+                {
+                    switch (test.at(b).toLatin1()) {
+                    case 'A':
+                        AValue++;
+                        break;
+                    case 'G':
+                        GValue++;
+                        break;
+                    case 'T':
+                        TValue++;
+                        break;
+                    case 'C':
+                        CValue++;
+                        break;
+                    default:
+                        notBaseValue++;
+                        break;
+                    }
+                }
+
+            }
+        }
+        ui->ALabel->setText(QString::number(AValue));
+        ui->GLabel->setText(QString::number(GValue));
+        ui->TLabel->setText(QString::number(TValue));
+        ui->CLabel->setText(QString::number(CValue));
+        ui->InvalidLabel->setText(QString::number(notBaseValue));
+
+        total = AValue + GValue + TValue + CValue;
+        ui->totalLabel->setText(QString::number(total));
+        if(total != 0)
+        {
+            double GPercent = ((double)GValue/(double)total) * 100.00;
+            ui->GPercent->setText("G Percentage: " + QString::number(GPercent) + "%");
+            double CPercent = ((double)CValue / (double)total) * 100.00;
+            ui->CPercent->setText("C Percentage: " + QString::number(CPercent) + "%");
+            double APercent = ((double)AValue / (double)total) * 100.00;
+            ui->APercent->setText("A Percentage: " + QString::number(APercent) + "%");
+            double TPercent = ((double)TValue / (double)total) * 100.00;
+            ui->TPercent->setText("T Percentage: " + QString::number(TPercent) + "%");
+        }
+
+    }
 }
 
--- qtDesignStudio/JenSeqUI/JenSeqUIContent/Screen01.ui.qml
+++ qtDesignStudio/JenSeqUI/JenSeqUIContent/Screen01.ui.qml
@@ -1,10 +1,11 @@
+
+
 /*
 This is a UI file (.ui.qml) that is intended to be edited in Qt Design Studio only.
 It is supposed to be strictly declarative and only uses a subset of QML. If you edit
 this file manually, you might introduce QML code that is not supported by Qt Design Studio.
 Check out https://doc.qt.io/qtcreator/creator-quick-ui-forms.html for details on .ui.qml files.
 */
-
 import QtQuick
 import QtQuick.Controls
 import JenSeqUI
@@ -12,12 +13,48 @@ import JenSeqUI
 Rectangle {
     width: Constants.width
     height: Constants.height
+    color: "#000000"
 
-    color: Constants.backgroundColor
+    Image {
+        id: jenseqlogo
+        x: 0
+        y: 8
+        width: 453
+        height: 201
+        source: "images/jenseqlogo.png"
+        fillMode: Image.PreserveAspectFit
+    }
 
     Text {
-        text: qsTr("Hello JenSeqUI")
-        anchors.centerIn: parent
-        font.family: Constants.font.family
+        id: text1
+        x: 108
+        y: 154
+        color: "#ffffff"
+        text: "Jenna's Sequencer"
+        font.pointSize: 20
+        font.family: "Jersey 10"
+    }
+
+    TextArea {
+        id: textArea
+        x: 80
+        y: 413
+        width: 770
+        height: 425
+        color: "#000000"
+        text: "Hello this is the text"
+        font.pointSize: 22
+        layer.format: ShaderEffectSource.Alpha
+        placeholderTextColor: "#d6ffffff"
+        placeholderText: qsTr("Text Area")
+
+        Rectangle {
+            id: rectangle
+            x: 149
+            y: -19
+            width: 770
+            height: 425
+            color: "#63c74d"
+        }
     }
 }
